d578aadf05b93c225a74f5a82abea76fba7bf236
vdp2025 <gracielegslouback@gmail.com>
2025-05-22 15:08:59 +0000

    botão

--- src/components/QuizIntro.tsx
+++ src/components/QuizIntro.tsx
@@ -1,4 +1,3 @@
-
 'use client';
 
 import React, { useState, useEffect } from 'react';
@@ -212,7 +211,7 @@ const QuizIntro: QuizIntroComponent = ({ onStart }) => {
                 placeholder="Digite seu nome"
                 value={nome}
                 onChange={(e) => setNome(e.target.value)}
-                className="w-full p-2.5 bg-[#FEFEFE] rounded-md border-[#B89B7A] focus:border-[#A1835D] focus:ring-[#A1835D]"
+                className="w-full p-2.5 bg-[#FEFEFE] rounded-md border-2 border-[#B89B7A] focus:border-2 focus:border-[#A1835D] focus:ring-[#A1835D]"
                 autoFocus
                 aria-required="true"
                 autoComplete="off"
