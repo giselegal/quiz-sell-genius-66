166e5b887ff1b59aad4c6fd6ac4de9ff6997ea66
vdp2025 <gracielegslouback@gmail.com>
2025-05-22 04:19:13 +0000

    barra de rolagem

--- src/data/questions/styleExperienceQuestions.ts
+++ src/data/questions/styleExperienceQuestions.ts
@@ -1,4 +1,3 @@
-
 import { QuizQuestion } from '../../types/quiz';
 
 export const styleExperienceQuestions: QuizQuestion[] = [
@@ -7,6 +6,7 @@ export const styleExperienceQuestions: QuizQuestion[] = [
     title: 'Como você aprende melhor sobre estilo e moda?',
     type: 'text',
     multiSelect: 1,
+    imageUrl: 'https://res.cloudinary.com/dqljyf76t/image/upload/v1746334748/ChatGPT_Image_4_de_mai._de_2025_01_40_38_qtv4xy.png',
     options: [
       {
         id: 'strategic-3-1',
@@ -35,6 +35,7 @@ export const styleExperienceQuestions: QuizQuestion[] = [
     title: 'O que você mais valoriza em um guia de estilo?',
     type: 'text',
     multiSelect: 1,
+    imageUrl: 'https://res.cloudinary.com/dqljyf76t/image/upload/v1746334756/ChatGPT_Image_4_de_mai._de_2025_01_42_42_jlugsc.webp',
     options: [
       {
         id: 'strategic-4-1',
