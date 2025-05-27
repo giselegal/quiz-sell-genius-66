b4d3d8e923c2df450e9475f74c0bd9e95363b82d
vdp2025 <gracielegslouback@gmail.com>
2025-05-27 23:16:21 +0000

    páginas

--- src/components/QuizFlow.tsx
+++ src/components/QuizFlow.tsx
@@ -2,7 +2,7 @@
 "use client";
 
 import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useUniversalNavigation } from '../hooks/useUniversalNavigation';
 
 type QuestionOption = {
   id: string;
