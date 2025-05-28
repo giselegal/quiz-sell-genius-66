428af4634fee788d1294ba48587d8e6cde21fe46
vdp2025 <gracielegslouback@gmail.com>
2025-05-28 20:19:21 +0000

    páginas

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,3 @@
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
@@ -22,7 +21,7 @@ export default defineConfig({
     minify: 'terser'
   },
   server: {
-    port: 3000,
+    port: 8080,
     open: true
   }
 });
