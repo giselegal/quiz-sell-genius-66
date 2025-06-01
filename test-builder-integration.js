5d915fc239b7e15d23882b30733954c199392d8f
vdp2025 <gracielegslouback@gmail.com>
2025-06-01 15:44:05 +0000

    correção questões estratégicas

--- test-builder-integration.js
+++ test-builder-integration.js
@@ -8,6 +8,8 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+console.log('🔧 Testando integração Builder.io...');
+
 const requiredFiles = [
   'src/utils/builderConfig.ts',
   'src/utils/builderComponentRegistry.ts', 
