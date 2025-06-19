82df83e9d1f1d33053c9b9c074e383a60975843a
vdp2025 <gracielegslouback@gmail.com>
2025-06-19 02:02:01 +0000

    Descrição das suas alterações

---
+++ src/components/visual-editor/TestCanvasArea.tsx
@@ -0,0 +1,12 @@
+// Teste simples para CanvasArea
+import React from "react";
+
+const TestCanvasArea = () => {
+  return (
+    <div>
+      <h1>Test CanvasArea</h1>
+    </div>
+  );
+};
+
+export default TestCanvasArea;
