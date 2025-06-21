b7216b0305e75f9d1dff9d6c56ee05720fa1fa7a
vdp2025 <gracielegslouback@gmail.com>
2025-06-21 00:06:31 +0000

    Descrição das suas alterações

---
+++ src/pages/editor-test.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import DragDropQuizEditor from "@/components/visual-editor/DragDropQuizEditor";
+
+const EditorTestPage: React.FC = () => {
+  return (
+    <div className="min-h-screen bg-background">
+      <DragDropQuizEditor />
+    </div>
+  );
+};
+
+export default EditorTestPage;
